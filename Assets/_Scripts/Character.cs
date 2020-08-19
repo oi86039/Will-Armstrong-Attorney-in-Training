23e63120a337253cab4ea3c295ee7eb22cf0ef4a
Omar <oai4@njit.edu>
2020-08-19 11:46:16 -0400

    Made UI Scalable and Build Ready. Lazygit added

--- Assets/_Scripts/Character.cs
+++ Assets/_Scripts/Character.cs
@@ -1,17 +1,20 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Character : MonoBehaviour {
+public class Character : MonoBehaviour
+{
 
-	public Sprite[] characterPoses = null;
+    public Sprite[] characterPoses = null;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
 
-	}
+    }
 
-	// Update is called once per frame
-	void Update () {
+    // Update is called once per frame
+    void Update()
+    {
 
-	}
+    }
 }
--- Assets/_Scripts/DialogueParser.cs
+++ Assets/_Scripts/DialogueParser.cs
@@ -1,144 +1,179 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEditor;
-using UnityEditor.SceneManagement;
+using UnityEngine.SceneManagement;
 using System.Text;
 using System.IO;
 using System.Text.RegularExpressions;
 using System.Collections.Generic;
 
-public class DialogueParser : MonoBehaviour {
+public class DialogueParser : MonoBehaviour
+{
 
-	struct DialogueLine {
-		public string name;
-		public string content;
-		public int pose;
-		public string position;
-		public string color;
-		public int background;
-		public int music;
-		public string[] options;
+    struct DialogueLine
+    {
+        public string name;
+        public string content;
+        public int pose;
+        public string position;
+        public string color;
+        public int background;
+        public int music;
+        public string[] options;
 
-		public DialogueLine(string Name, string Content, int Pose, string Position, string Color, int Background, int Music) {
-			name = Name;
-			content = Content;
-			pose = Pose;
-			position = Position;
-			color = Color;
-			background = Background;
-			music = Music;
-			options = new string[0];
-		}
-	}
+        public DialogueLine(string Name, string Content, int Pose, string Position, string Color, int Background, int Music)
+        {
+            name = Name;
+            content = Content;
+            pose = Pose;
+            position = Position;
+            color = Color;
+            background = Background;
+            music = Music;
+            options = new string[0];
+        }
+    }
 
-	List<DialogueLine> lines;
+    List<DialogueLine> lines;
 
-	// Use this for initialization
-	void Start () {
-		string file = "Assets/Data/Dialogue";
-		string sceneNum = EditorSceneManager.GetActiveScene().name;
-		sceneNum = Regex.Replace (sceneNum, "[^0-9]", "");
-		file += sceneNum;
-		file += ".txt";
+    // Use this for initialization
+    void Start()
+    {
+        string file = "/Dialogue";
+        string sceneNum = SceneManager.GetActiveScene().name;
+        sceneNum = Regex.Replace(sceneNum, "[^0-9]", "");
+        file += sceneNum;
+        file += ".txt";
 
-		lines = new List<DialogueLine>();
+        lines = new List<DialogueLine>();
 
-		LoadDialogue (file);
-	}
+        LoadDialogue(file);
+    }
 
-	// Update is called once per frame
-	void Update () {
+    // Update is called once per frame
+    void Update()
+    {
 
-	}
+    }
 
-	void LoadDialogue(string filename) {
-		string line;
-		StreamReader r = new StreamReader (filename);
+    void LoadDialogue(string filename)
+    {
+        string line;
+        StreamReader r = new StreamReader(Application.streamingAssetsPath + filename);
 
-		using (r) {
-			do {
-				line = r.ReadLine();
-				if (line != null) {
-					string[] lineData = line.Split(';');
-					if (lineData[0] == "Player") {
-						DialogueLine lineEntry = new DialogueLine(lineData[0], "", 0, "","",0,0);
-						lineEntry.options = new string[lineData.Length-1];
-						for (int i = 1; i < lineData.Length; i++) {
-							lineEntry.options[i-1] = lineData[i];
-						}
-						lines.Add(lineEntry);
-					} else if (lineData[0] == "Event") {
-						DialogueLine lineEntry = new DialogueLine(lineData[0], lineData[1], 0, "","G",int.Parse(lineData[2]),int.Parse(lineData[3]));
-						lines.Add(lineEntry);
-					} else if (lineData[0] == "Evidence") {
-						DialogueLine lineEntry = new DialogueLine(lineData[0], lineData[1], 0, "M","Y",int.Parse(lineData[2]),int.Parse(lineData[3]));
-						lines.Add(lineEntry);
-					} else if (lineData[0] == "Health") {
-						DialogueLine lineEntry = new DialogueLine(lineData[0], "You've lost 20 health...", 0, "M","R",int.Parse(lineData[1]),int.Parse(lineData[2]));
-						lines.Add(lineEntry);
-					} else {
-						DialogueLine lineEntry = new DialogueLine(lineData[0], lineData[1], int.Parse(lineData[2]), lineData[3], lineData[4], int.Parse(lineData[5]), int.Parse(lineData[6]));
-						lines.Add(lineEntry);
-					}
-				}
-			}
-			while (line != null);
-			r.Close();
-		}
-	}
+        using (r)
+        {
+            do
+            {
+                line = r.ReadLine();
+                if (line != null)
+                {
+                    string[] lineData = line.Split(';');
+                    if (lineData[0] == "Player")
+                    {
+                        DialogueLine lineEntry = new DialogueLine(lineData[0], "", 0, "", "", 0, 0);
+                        lineEntry.options = new string[lineData.Length - 1];
+                        for (int i = 1; i < lineData.Length; i++)
+                        {
+                            lineEntry.options[i - 1] = lineData[i];
+                        }
+                        lines.Add(lineEntry);
+                    }
+                    else if (lineData[0] == "Event")
+                    {
+                        DialogueLine lineEntry = new DialogueLine(lineData[0], lineData[1], 0, "", "G", int.Parse(lineData[2]), int.Parse(lineData[3]));
+                        lines.Add(lineEntry);
+                    }
+                    else if (lineData[0] == "Evidence")
+                    {
+                        DialogueLine lineEntry = new DialogueLine(lineData[0], lineData[1], 0, "M", "Y", int.Parse(lineData[2]), int.Parse(lineData[3]));
+                        lines.Add(lineEntry);
+                    }
+                    else if (lineData[0] == "Health")
+                    {
+                        DialogueLine lineEntry = new DialogueLine(lineData[0], "You've lost 20 health...", 0, "M", "R", int.Parse(lineData[1]), int.Parse(lineData[2]));
+                        lines.Add(lineEntry);
+                    }
+                    else
+                    {
+                        DialogueLine lineEntry = new DialogueLine(lineData[0], lineData[1], int.Parse(lineData[2]), lineData[3], lineData[4], int.Parse(lineData[5]), int.Parse(lineData[6]));
+                        lines.Add(lineEntry);
+                    }
+                }
+            }
+            while (line != null);
+            r.Close();
+        }
+    }
 
-	public string GetPosition(int lineNumber) {
-		if (lineNumber < lines.Count) {
-			return lines[lineNumber].position;
-		}
-		return "";
-	}
+    public string GetPosition(int lineNumber)
+    {
+        if (lineNumber < lines.Count)
+        {
+            return lines[lineNumber].position;
+        }
+        return "";
+    }
 
-	public string GetName(int lineNumber) {
-		if (lineNumber < lines.Count) {
-			return lines[lineNumber].name;
-		}
-		return "";
-	}
+    public string GetName(int lineNumber)
+    {
+        if (lineNumber < lines.Count)
+        {
+            return lines[lineNumber].name;
+        }
+        return "";
+    }
 
-	public string GetContent(int lineNumber) {
-		if (lineNumber < lines.Count) {
-			return lines[lineNumber].content;
-		}
-		return "";
-	}
+    public string GetContent(int lineNumber)
+    {
+        if (lineNumber < lines.Count)
+        {
+            return lines[lineNumber].content;
+        }
+        return "";
+    }
 
-	public int GetPose(int lineNumber) {
-		if (lineNumber < lines.Count) {
-			return lines[lineNumber].pose;
-		}
-		return 0;
-	}
+    public int GetPose(int lineNumber)
+    {
+        if (lineNumber < lines.Count)
+        {
+            return lines[lineNumber].pose;
+        }
+        return 0;
+    }
 
-	public string[] GetOptions(int lineNumber) {
-		if (lineNumber < lines.Count) {
-			return lines[lineNumber].options;
-		}
-		return new string[0];
-	}
+    public string[] GetOptions(int lineNumber)
+    {
+        if (lineNumber < lines.Count)
+        {
+            return lines[lineNumber].options;
+        }
+        return new string[0];
+    }
 
-	public string GetColor(int lineNumber){
-		if (lineNumber < lines.Count) {
-			return lines[lineNumber].color;
-		}
-		return "";
-}
-	public int GetBackground(int lineNumber){
-		if (lineNumber < lines.Count) {
-			return lines[lineNumber].background;
-		}
-		return 0;
-	}
+    public string GetColor(int lineNumber)
+    {
+        if (lineNumber < lines.Count)
+        {
+            return lines[lineNumber].color;
+        }
+        return "";
+    }
+    public int GetBackground(int lineNumber)
+    {
+        if (lineNumber < lines.Count)
+        {
+            return lines[lineNumber].background;
+        }
+        return 0;
+    }
 
-	public int GetMusic(int lineNumber){
-		if (lineNumber < lines.Count) {
-			return lines[lineNumber].music;
-		}
-		return 0;
-	}
+    public int GetMusic(int lineNumber)
+    {
+        if (lineNumber < lines.Count)
+        {
+            return lines[lineNumber].music;
+        }
+        return 0;
+    }
 }
